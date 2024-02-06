46bc57d5412f5f6f90113cffd27c746bf1e14766
vargbalazs <balazs.varga.8564@gmail.com>
2024-02-06 19:58:13 +0100

    really finishing frozen columns

--- src/app/directives/methods/scroll-to-column-mouse.ts
+++ src/app/directives/methods/scroll-to-column-mouse.ts
@@ -1,10 +1,13 @@
 import { EnhancedGridConfig } from '../classes/enhanced-grid-config.class';
 
-// scrolls the grid, if we are selectin with the mouse and there are frozen columns
+// scrolls the grid, if we are selecting with the mouse and there are frozen columns
 export function scrollToColumnMouse(config: EnhancedGridConfig) {
   const gridContent = (<HTMLElement>(
     config.gridElRef.nativeElement
   )).querySelector('.k-grid-content');
+  const nextCell = gridContent?.querySelector(
+    `[ng-reflect-data-row-index="${config.lastSelectedCell.itemKey}"][ng-reflect-col-index="${config.lastSelectedCell.columnKey}"]`
+  );
   // if the grid content is scrolled and we select to the right from any of the frozen columns, then scroll
   if (
     config.firstSelectedCell.columnKey <= config.frozenColumns.length - 1 &&
@@ -16,10 +19,21 @@ export function scrollToColumnMouse(config: EnhancedGridConfig) {
       behavior: 'smooth',
     });
   }
-  // if we select to the left
+  // if we select to the right at the end
+  if (
+    nextCell?.getBoundingClientRect().right! >
+    gridContent?.getBoundingClientRect().right!
+  ) {
+    gridContent?.scrollBy({
+      left: nextCell?.getBoundingClientRect().width,
+      behavior: 'smooth',
+    });
+  }
+  // if we select to the left and the content is scrolled
   if (
     config.lastSelectedCell.columnKey <= config.frozenColumns.length - 1 &&
-    gridContent!.scrollLeft > 0
+    gridContent!.scrollLeft > 0 &&
+    config.firstSelectedCell.columnKey > config.frozenColumns.length - 1
   ) {
     gridContent?.scrollBy({
       left: -gridContent.scrollLeft,
