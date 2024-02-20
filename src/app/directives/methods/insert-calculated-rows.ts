d95c9efddc73d2742c34908262054c4323a8febf
vargbalazs <balazs.varga.8564@gmail.com>
2024-02-20 21:11:57 +0100

    working on calc grid III

--- src/app/directives/methods/insert-calculated-rows.ts
+++ src/app/directives/methods/insert-calculated-rows.ts
@@ -1,4 +1,4 @@
-import { GridComponent, GridDataResult } from '@progress/kendo-angular-grid';
+import { GridComponent, RowClassArgs } from '@progress/kendo-angular-grid';
 import { CalculatedRow } from '../interfaces/calculated-row.interface';
 import { EnhancedGridConfig } from '../classes/enhanced-grid-config.class';
 
@@ -37,28 +37,39 @@ export function insertCalculatedRows(
     } else {
       rowData[calcRow.title.writeToField] = calcRow.title.value;
     }
+    // mark the row as calculated
+    rowData.calculated = true;
     // insert the row
     gridData.splice(lastIndex + 1, 0, rowData);
-
     // set the style of the unused fields
     // we need setTimeout in order to have the data changes reflected in the dom
     setTimeout(() => {
-      const gridBody = (<HTMLElement>(
-        config.gridElRef.nativeElement
-      )).querySelector('[kendogridtablebody]');
       for (let i = 0; i <= config.columns.length - 1; i++) {
         if (
           config.columns[i].field !== calcRow.title.writeToField &&
           !calcRow.calculatedFields.includes(config.columns[i].field)
         ) {
-          let target = gridBody!.querySelector(
-            `[ng-reflect-data-row-index="${
-              lastIndex + 1
-            }"][ng-reflect-col-index="${i}"]`
-          );
-          target?.classList.add('calcrow-unused-cell');
+          // get the existing cssClass property of the column
+          let cssClass = config.columns[i].cssClass;
+          // add the existing css class or classes to an array
+          const cssClasses: string[] = [];
+          if (cssClass && typeof cssClass === 'string')
+            cssClasses.push(cssClass);
+          if (cssClass && Array.isArray(cssClass)) cssClasses.push(...cssClass);
+          // add the custom class
+          cssClasses.push('hide-value');
+          // overwrite the cssClass property with the new array
+          config.columns[i].cssClass = cssClasses;
         }
       }
     });
   });
+  // pass the styling callback to the grid
+  grid.rowClass = rowCallback;
+}
+
+// callback for styling the unused cells in a calculated row
+function rowCallback(context: RowClassArgs) {
+  if (context.dataItem.calculated) return { 'calcrow-unused-cell': true };
+  return '';
 }
