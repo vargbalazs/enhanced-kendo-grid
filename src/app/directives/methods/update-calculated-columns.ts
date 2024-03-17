612a0d3fc6a186311450610816f132121ab3653f
vargbalazs <balazs.varga.8564@gmail.com>
2024-03-17 09:19:31 +0100

    handling dot fields for calculated columns

--- src/app/directives/methods/update-calculated-columns.ts
+++ src/app/directives/methods/update-calculated-columns.ts
@@ -1,16 +1,26 @@
 import { EnhancedGridConfig } from '../classes/enhanced-grid-config.class';
+import * as methods from './index';
 
 // updates the calculated columns
 export function updateCalculatedColumns(config: EnhancedGridConfig) {
   // loop through the calc cols
   config.colCalculation.calculatedColumns.forEach((calcCol) => {
+    const keyAndFieldCalcCol = methods.extractKeyAndField(calcCol.field);
     switch (calcCol.calculateFunction) {
       case 'sum':
         config.gridData.forEach((row) => {
           let result = 0;
           calcCol.calculateByColumns.forEach((col) => {
-            result += +row[col];
-            row[calcCol.field] = result;
+            const keyAndField = methods.extractKeyAndField(col);
+            result += keyAndField.fieldName
+              ? +row[keyAndField.key][keyAndField.fieldName]
+              : +row[col];
+            if (keyAndFieldCalcCol.fieldName) {
+              row[keyAndFieldCalcCol.key][keyAndFieldCalcCol.fieldName] =
+                result;
+            } else {
+              row[calcCol.field] = result;
+            }
           });
         });
         break;
@@ -18,8 +28,16 @@ export function updateCalculatedColumns(config: EnhancedGridConfig) {
         config.gridData.forEach((row) => {
           let result = 0;
           calcCol.calculateByColumns.forEach((col) => {
-            result += +row[col];
-            row[calcCol.field] = result / calcCol.calculateByColumns.length;
+            const keyAndField = methods.extractKeyAndField(col);
+            result += keyAndField.fieldName
+              ? +row[keyAndField.key][keyAndField.fieldName]
+              : +row[col];
+            if (keyAndFieldCalcCol.fieldName) {
+              row[keyAndFieldCalcCol.key][keyAndFieldCalcCol.fieldName] =
+                result / calcCol.calculateByColumns.length;
+            } else {
+              row[calcCol.field] = result / calcCol.calculateByColumns.length;
+            }
           });
         });
         break;
@@ -27,29 +45,52 @@ export function updateCalculatedColumns(config: EnhancedGridConfig) {
         config.gridData.forEach((row) => {
           let values: number[] = [];
           calcCol.calculateByColumns.forEach((col) => {
-            values.push(+row[col]);
+            const keyAndField = methods.extractKeyAndField(col);
+            values.push(
+              keyAndField.fieldName
+                ? +row[keyAndField.key][keyAndField.fieldName]
+                : +row[col]
+            );
           });
           let result = values.reduce((prev, curr) =>
             prev < curr ? prev : curr
           );
-          row[calcCol.field] = result;
+          if (keyAndFieldCalcCol.fieldName) {
+            row[keyAndFieldCalcCol.key][keyAndFieldCalcCol.fieldName] = result;
+          } else {
+            row[calcCol.field] = result;
+          }
         });
         break;
       case 'max':
         config.gridData.forEach((row) => {
           let values: number[] = [];
           calcCol.calculateByColumns.forEach((col) => {
-            values.push(+row[col]);
+            const keyAndField = methods.extractKeyAndField(col);
+            values.push(
+              keyAndField.fieldName
+                ? +row[keyAndField.key][keyAndField.fieldName]
+                : +row[col]
+            );
           });
           let result = values.reduce((prev, curr) =>
             prev > curr ? prev : curr
           );
-          row[calcCol.field] = result;
+          if (keyAndFieldCalcCol.fieldName) {
+            row[keyAndFieldCalcCol.key][keyAndFieldCalcCol.fieldName] = result;
+          } else {
+            row[calcCol.field] = result;
+          }
         });
         break;
       case 'count':
         config.gridData.forEach((row) => {
-          row[calcCol.field] = calcCol.calculateByColumns.length;
+          if (keyAndFieldCalcCol.fieldName) {
+            row[keyAndFieldCalcCol.key][keyAndFieldCalcCol.fieldName] =
+              calcCol.calculateByColumns.length;
+          } else {
+            row[calcCol.field] = calcCol.calculateByColumns.length;
+          }
         });
         break;
     }
