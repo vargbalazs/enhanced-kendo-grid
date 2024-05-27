929637ea0f9d2751c17ae4b7374cefec66eac736
vargbalazs <balazs.varga.8564@gmail.com>
2024-05-27 10:24:35 +0200

    exporting the directive itself

--- projects/enhanced-k-grid/src/public-api.ts
+++ projects/enhanced-k-grid/src/public-api.ts
@@ -1,4 +1,5 @@
 export * from './lib/enhanced-grid-directive.module';
+export * from './lib/enhanced-grid.directive';
 export * from './lib/interfaces/aggregate.interface';
 export * from './lib/interfaces/list-source.interface';
 export * from './lib/interfaces/form-error-message.interface';
