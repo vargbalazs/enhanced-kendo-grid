14b79a042fcc1fd3c6339e15710e42804517aac3
vargbalazs <balazs.varga.8564@gmail.com>
2024-07-31 21:05:59 +0200

    revisiting sorted column style method

--- projects/enhanced-k-grid/src/lib/methods/toggle-sorted-column-class.ts
+++ projects/enhanced-k-grid/src/lib/methods/toggle-sorted-column-class.ts
@@ -8,35 +8,37 @@ export function toggleSortedColumnClass(
 ) {
   // if there is already one sorted column, then remove the style from it
   if (config.lastSortedColIndex >= 0) {
-    const cells = getSortedCells(config, config.lastSortedColIndex);
-    cells?.forEach((cell) => {
-      cell.classList.remove('col-sorted');
-    });
-    // reset lastSortedColIndex
-    config.lastSortedColIndex = -1;
+    // get the existing cssClass property of the column
+    const colCssClass = config.columns[config.lastSortedColIndex].cssClass;
+    removeSortedClass(colCssClass, config, config.lastSortedColIndex);
   }
   // get the column index of the sorted column
   const field = sortChangeEvent[0].field;
   const colIndex = config.columns.findIndex((col) => col.field === field);
-  // query for the cells in the column
-  const cells = getSortedCells(config, colIndex);
+  // get the existing cssClass property of the column
+  const colCssClass = config.columns[colIndex].cssClass;
+  // add the existing css class or classes to an array
+  const cssClasses: string[] = [];
   // add or remove the sorted class to/from the cells
   if (sortChangeEvent[0].dir) {
-    cells?.forEach((cell) => {
-      cell.classList.add('col-sorted');
-    });
     // store it as the last sorted column
     config.lastSortedColIndex = colIndex;
+    // if there is only one css class
+    if (colCssClass && typeof colCssClass === 'string')
+      cssClasses.push(colCssClass);
+    // if there are multiple css classes
+    if (colCssClass && Array.isArray(colCssClass))
+      cssClasses.push(...colCssClass);
+    // add the sorted class
+    cssClasses.push('col-sorted');
+    // overwrite the cssClass property with the new array
+    config.columns[colIndex].cssClass = cssClasses;
   } else {
-    cells?.forEach((cell) => {
-      cell.classList.remove('col-sorted');
-    });
-    // reset lastSortedColIndex
-    config.lastSortedColIndex = -1;
+    removeSortedClass(colCssClass, config, colIndex);
   }
 }
 
-function getSortedCells(
+function getColumnCells(
   config: EnhancedGridConfig,
   colIndex: number
 ): NodeListOf<Element> | undefined {
@@ -48,4 +50,18 @@ function getSortedCells(
     `[ng-reflect-col-index="${colIndex}"]`
   );
   return cells;
+}
+
+function removeSortedClass(
+  colCssClass: string | string[] | Set<string> | { [key: string]: any },
+  config: EnhancedGridConfig,
+  colIndex: number
+) {
+  if (typeof colCssClass === 'string') config.columns[colIndex].cssClass = '';
+  if (Array.isArray(colCssClass))
+    config.columns[colIndex].cssClass = (<string[]>colCssClass).filter(
+      (cssClass) => cssClass != 'col-sorted'
+    );
+  // reset lastSortedColIndex
+  config.lastSortedColIndex = -1;
 }
