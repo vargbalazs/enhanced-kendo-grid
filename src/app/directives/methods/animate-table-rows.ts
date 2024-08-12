f5f3b2f8c61fc1edae38628277acde638c14182e
vargbalazs <balazs.varga.8564@gmail.com>
2024-08-12 21:07:40 +0200

    working on group rows

--- src/app/directives/methods/animate-table-rows.ts
+++ src/app/directives/methods/animate-table-rows.ts
@@ -12,38 +12,59 @@ export function animateTableRows(
   const calcRow = (<HTMLElement>config.gridElRef.nativeElement).querySelector(
     `[kendogridlogicalrow].${calcRowName}`
   );
-  const dataRowIndex = +calcRow!.getAttribute('ng-reflect-data-row-index')!;
-  // calculate the index, from wich we want to slide up/slide down the table rows
-  const rowIndexesCount = config.rowCalculation.calculatedRows.find(
-    (calcRow) => calcRow.name === calcRowName
-  )?.rowIndexes?.length;
-  const fromRowIndex = dataRowIndex + rowIndexesCount! + 1;
-  // calculate the y value for translateY (theoretically it is always the same, but who knows)
-  let totalHeight = 0;
-  for (let i = dataRowIndex + 1; i <= rowIndexesCount!; i++) {
-    const row = (<HTMLElement>config.gridElRef.nativeElement).querySelector(
-      `[ng-reflect-data-row-index='${i}']`
-    );
-    totalHeight += row!.getBoundingClientRect().height;
-  }
-  // query for all rows below this index (inclusive)
-  for (let i = fromRowIndex; i <= config.gridData.length - 1; i++) {
-    const row = (<HTMLElement>config.gridElRef.nativeElement).querySelector(
-      `[ng-reflect-data-row-index='${i}']`
-    );
-    // slide up or down the rows
-    switch (state) {
-      case 'expanded':
-        renderer2.setStyle(row, 'transform', 'translateY(0px)', 2);
-        break;
-      case 'collapsed':
-        renderer2.setStyle(
-          row,
-          'transform',
-          `translateY(-${totalHeight}px)`,
-          2
-        );
-        break;
-    }
+  // const dataRowIndex = +calcRow!.getAttribute('ng-reflect-data-row-index')!;
+  // // calculate the index, from wich we want to slide up/slide down the table rows
+  // const rowIndexesCount = config.rowCalculation.calculatedRows.find(
+  //   (calcRow) => calcRow.name === calcRowName
+  // )?.rowIndexes?.length;
+  // const fromRowIndex = dataRowIndex + rowIndexesCount! + 1;
+  // // calculate the y value for translateY (theoretically it is always the same, but who knows)
+  // let totalHeight = 0;
+  // for (let i = dataRowIndex + 1; i <= rowIndexesCount!; i++) {
+  //   const row = (<HTMLElement>config.gridElRef.nativeElement).querySelector(
+  //     `[ng-reflect-data-row-index='${i}']`
+  //   );
+  //   totalHeight += row!.getBoundingClientRect().height;
+  // }
+  // // query for all rows below this index (inclusive)
+  // for (let i = fromRowIndex; i <= config.gridData.length - 1; i++) {
+  //   const row = (<HTMLElement>config.gridElRef.nativeElement).querySelector(
+  //     `[ng-reflect-data-row-index='${i}']`
+  //   );
+  //   // slide up or down the rows
+  //   switch (state) {
+  //     case 'expanded':
+  //       renderer2.setStyle(row, 'transform', 'translateY(0px)', 2);
+  //       break;
+  //     case 'collapsed':
+  //       renderer2.setStyle(
+  //         row,
+  //         'transform',
+  //         `translateY(-${totalHeight}px)`,
+  //         2
+  //       );
+  //       break;
+  //   }
+  // }
+  switch (state) {
+    case 'expanded':
+      $(`[kendogridlogicalrow][calcrowname=${calcRowName}] td`)
+        .animate({ padding: 0 })
+        .wrapInner('<div />')
+        .children()
+        .slideDown(function () {
+          $(this).closest('tr').show();
+        });
+      break;
+    case 'collapsed':
+      $(`[kendogridlogicalrow][calcrowname=${calcRowName}]`)
+        .animate({ paddingTop: 0, paddingBottom: 0 }, 500)
+        .wrapInner('<div />')
+        .children()
+        .slideUp(500, function () {
+          $(this).closest('tr').hide();
+        });
+
+      break;
   }
 }
