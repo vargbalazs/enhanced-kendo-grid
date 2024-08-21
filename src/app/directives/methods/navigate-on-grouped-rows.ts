6a04a191ccc6d48cf2146e8c6a682a8724588a45
vargbalazs <balazs.varga.8564@gmail.com>
2024-08-21 20:08:25 +0200

    working on navigate on grouped rows

---
+++ src/app/directives/methods/navigate-on-grouped-rows.ts
@@ -0,0 +1,9 @@
+// handles the cases if we press simple arrow buttons in a grouped grid
+
+import { EnhancedGridConfig } from '../classes/enhanced-grid-config.class';
+
+// in this case we have to jump to the next calc row (down/up), if the source calc row is collapsed
+export function navigateOnGroupedRows(
+  e: KeyboardEvent,
+  config: EnhancedGridConfig
+) {}
