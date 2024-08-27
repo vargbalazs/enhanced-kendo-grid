3746945178c49b9c2abf4ea6b68045d449395685
vargbalazs <balazs.varga.8564@gmail.com>
2024-08-27 20:42:13 +0200

    still working on multiple levels 3

--- src/app/directives/methods/get-child-row-indexes.ts
+++ src/app/directives/methods/get-child-row-indexes.ts
@@ -11,9 +11,10 @@ export function getChildRowIndexes(
   // define empty array for the indexes
   const rowIndexes: number[] = [];
   // process 'filteredData'
+  // the 'filteredData' array contains already the 2. level group rows
   for (let i = 0; i <= filteredData.length - 1; i++) {
     const row = filteredData[i];
-    // if the filteredData row is a calculated one, then search for it differently
+    // if the filteredData row is a calculated one, then search for the row index differently
     if (row.calculated) {
       const gridRow = (<HTMLElement>(
         config.gridElRef.nativeElement
@@ -21,12 +22,23 @@ export function getChildRowIndexes(
       const rowIndex = +gridRow?.getAttribute('ng-reflect-data-row-index')!;
       rowIndexes.push(rowIndex);
       // search for any child calculated rows
-      const childCalcRows = config.rowCalculation.calculatedRows.forEach(
+      // this rows will be on group level 3
+      const childCalcRows = config.rowCalculation.calculatedRows.filter(
         (r) => r.parentRowName === row.calcRowName
       );
-      console.log(childCalcRows);
+      if (childCalcRows.length > 0) {
+        childCalcRows.forEach((childCalcRow) => {
+          const gridRow = (<HTMLElement>(
+            config.gridElRef.nativeElement
+          )).querySelector(`[kendogridlogicalrow].${childCalcRow.name}`);
+          const rowIndex = +gridRow?.getAttribute('ng-reflect-data-row-index')!;
+          rowIndexes.push(rowIndex);
+        });
+      } else {
+        console.log(row);
+      }
     } else {
-      // if not, then search the grid data
+      // if not, then loop through the grid data to get the row index
       const rowIndex = config.gridData.findIndex(
         (dataRow) => dataRow.dataRowIndex === row.dataRowIndex
       );
