05eb18e95cb077894e01d7f1718f80b28bd2fa75
vargbalazs <balazs.varga.8564@gmail.com>
2024-09-15 19:37:47 +0200

    lib - still working on grp lvl btn 4

--- projects/enhanced-k-grid/src/lib/methods/group-level-btn-click.ts
+++ projects/enhanced-k-grid/src/lib/methods/group-level-btn-click.ts
@@ -15,9 +15,57 @@ export function groupLevelBtnClick(
     groupBtnState === 'expanded' ? 'collapsed' : 'expanded';
   btn.setAttribute('state', newGroupBtnState);
   // get the group indicators for this level and perform a click action
+  // const indicators = (<HTMLElement>(
+  //   config.gridElRef.nativeElement
+  // )).querySelectorAll(`.group-indicator[level="${groupBtnLevel}"]`);
+  // indicators.forEach((indicator) => {
+  //   // (<HTMLElement>indicator).click();
+  //   // get the actual state for the indicator/calc row
+  //   const indicatorState = indicator.getAttribute('state');
+  //   // toggle the state only if the indicator/calc row hasn't the new state already
+  //   if (indicatorState !== newGroupBtnState) {
+  //     methods.toggleCalcRowState(indicator, renderer2, config);
+  //   }
+  // });
+
+  if (groupBtnLevel === 1 && newGroupBtnState === 'collapsed') {
+    toggleLevel(config, 3, newGroupBtnState, renderer2);
+    setTimeout(() => {
+      toggleLevel(config, 2, newGroupBtnState, renderer2);
+      setTimeout(() => {
+        toggleLevel(config, 1, newGroupBtnState, renderer2);
+      }, 700);
+    }, 700);
+  }
+
+  if (groupBtnLevel === 2 && newGroupBtnState === 'collapsed') {
+    toggleLevel(config, 3, newGroupBtnState, renderer2);
+    setTimeout(() => {
+      toggleLevel(config, 2, newGroupBtnState, renderer2);
+    }, 700);
+  }
+
+  // if (groupBtnLevel === 1 && newGroupBtnState === 'expanded') {
+  //   toggleLevel(config, 1, newGroupBtnState, renderer2);
+  //   setTimeout(() => {
+  //     toggleLevel(config, 2, newGroupBtnState, renderer2);
+  //     setTimeout(() => {
+  //       toggleLevel(config, 3, newGroupBtnState, renderer2);
+  //     }, 700);
+  //   }, 700);
+  // }
+}
+
+function toggleLevel(
+  config: EnhancedGridConfig,
+  level: number,
+  newGroupBtnState: string,
+  renderer2: Renderer2
+) {
+  // get the group indicators for this level and perform a click action
   const indicators = (<HTMLElement>(
     config.gridElRef.nativeElement
-  )).querySelectorAll(`.group-indicator[level="${groupBtnLevel}"]`);
+  )).querySelectorAll(`.group-indicator[level="${level.toString()}"]`);
   indicators.forEach((indicator) => {
     // (<HTMLElement>indicator).click();
     // get the actual state for the indicator/calc row
