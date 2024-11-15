7d16d113b6cde6b18407d32df81578abebc76975
vargbalazs <balazs.varga.8564@gmail.com>
2024-11-15 15:37:13 +0100

    working on info tooltip 3

--- src/app/directives/methods/paint-info-in-cell.ts
+++ src/app/directives/methods/paint-info-in-cell.ts
@@ -1,8 +1,12 @@
 import { EnhancedGridConfig } from '../classes/enhanced-grid-config.class';
-import * as methods from './index';
+import { InfoTooltip } from '../interfaces/info-tooltip.interface';
 
 // paints an info icon in the given cell
-export function paintInfoInCell(cell: Element, config: EnhancedGridConfig) {
+export function paintInfoInCell(
+  cell: Element,
+  config: EnhancedGridConfig,
+  tooltip: InfoTooltip
+) {
   // get grid content
   const gridContent = (<HTMLElement>(
     config.gridElRef.nativeElement
@@ -11,14 +15,44 @@ export function paintInfoInCell(cell: Element, config: EnhancedGridConfig) {
   const iconDiv = document.createElement('div');
   iconDiv.innerHTML = 'i';
   iconDiv.setAttribute('info-icon', '');
+  const rowIndex = +cell.getAttribute('ng-reflect-data-row-index')!;
+  const colIndex = +cell.getAttribute('ng-reflect-col-index')!;
+  iconDiv.setAttribute('row-index', rowIndex.toString());
+  iconDiv.setAttribute('col-index', colIndex.toString());
+  iconDiv.setAttribute(
+    'start-top',
+    `${
+      cell.getBoundingClientRect().top - gridContent.getBoundingClientRect().top
+    }`
+  );
+  iconDiv.setAttribute(
+    'start-left',
+    `${
+      cell.getBoundingClientRect().left -
+      gridContent.getBoundingClientRect().left
+    }`
+  );
   iconDiv.classList.add('info-icon');
   // set the position
-  iconDiv.style.top = `${
-    cell.getBoundingClientRect().top - gridContent.getBoundingClientRect().top
-  }px`;
-  iconDiv.style.left = `${
-    cell.getBoundingClientRect().left - gridContent.getBoundingClientRect().left
-  }px`;
+  // if the icon is in a frozen column
+  if (colIndex < config.frozenColumns.length - 1) {
+    iconDiv.style.position = 'fixed';
+    iconDiv.style.top = `${
+      cell.getBoundingClientRect().top - gridContent.getBoundingClientRect().top
+    }px`;
+    iconDiv.style.left = `${
+      cell.getBoundingClientRect().left -
+      gridContent.getBoundingClientRect().left
+    }px`;
+  } else {
+    iconDiv.style.top = `${
+      cell.getBoundingClientRect().top - gridContent.getBoundingClientRect().top
+    }px`;
+    iconDiv.style.left = `${
+      cell.getBoundingClientRect().left -
+      gridContent.getBoundingClientRect().left
+    }px`;
+  }
   // append to the grid content
   gridContent.appendChild(iconDiv);
 }
--- src/app/directives/methods/scroll-info-icons.ts
+++ src/app/directives/methods/scroll-info-icons.ts
@@ -21,19 +21,30 @@ export function scrollInfoIcons(
       config.columns[config.frozenColumns[i].columnIndex!].width;
   }
   listener = renderer2.listen(gridContent, 'scroll', (e) => {
-    config.infoTooltips.forEach((tooltip) => {
-      const colIndex = config.columns.findIndex(
-        (col) => col.field === tooltip.columnField
-      );
+    icons.forEach((icon) => {
+      const colIndex = +icon.getAttribute('col-index')!;
+      const startTop = +icon.getAttribute('start-top')!;
+      const startLeft = +icon.getAttribute('start-left')!;
       // if we are in a non-frozen column
       if (colIndex > config.frozenColumns.length - 1) {
         // if we are beneath a frozen column (left side), then hide it
         if (
-          icons[0].getBoundingClientRect().left -
+          icon.getBoundingClientRect().left -
             gridContent.getBoundingClientRect().left <
           totalWidthFrozenCol
         ) {
-          (<HTMLElement>icons[0]).style.zIndex = '0';
+          (<HTMLElement>icon).style.zIndex = '0';
+        }
+      } else {
+        // if we are in a frozen column and scroll left/right, we have to fix the icons
+        if (gridContent.scrollLeft > 0) {
+          //(<HTMLElement>icon).style.position = 'fixed';
+          // (<HTMLElement>icon).style.top = `${
+          //   startTop + gridContent.getBoundingClientRect().top
+          // }px`;
+          // (<HTMLElement>icon).style.left = `${
+          //   startLeft + gridContent.getBoundingClientRect().left
+          // }px`;
         }
       }
     });
