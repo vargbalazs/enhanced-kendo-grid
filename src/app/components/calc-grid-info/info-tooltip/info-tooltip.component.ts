ed730b0202e41444cbacbf5cae78fd86f3d28e19
vargbalazs <balazs.varga.8564@gmail.com>
2024-12-02 14:58:39 +0100

    working on info tooltip 9

--- src/app/components/calc-grid-info/info-tooltip/info-tooltip.component.ts
+++ src/app/components/calc-grid-info/info-tooltip/info-tooltip.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { EnhancedGridToolTipComponent } from 'src/app/directives/enhanced-grid-tooltip.component';
 
 @Component({
   selector: 'info-tooltip',
   templateUrl: './info-tooltip.component.html',
   styleUrls: ['./info-tooltip.component.css'],
 })
-export class InfoTooltipComponent {}
+export class InfoTooltipComponent extends EnhancedGridToolTipComponent {}
