709552f034ef08550be5508c1a96e0feb0e70b45
vargbalazs <balazs.varga.8564@gmail.com>
2024-12-04 16:53:40 +0100

    working on info tooltip 10

---
+++ src/app/directives/methods/get-cell-value.ts
@@ -0,0 +1,31 @@
+import * as methods from './index';
+
+// gets the value of a given cell
+// first we search the corresponding row based on the rowField and rowValue parameters
+// then we get the cell value based on the columnField parameter
+export function getCellValue(
+  rowField: string,
+  rowValue: any,
+  columnField: string,
+  gridData: any[]
+): any {
+  const keyAndFieldRow = methods.extractKeyAndField(rowField);
+  const keyAndFieldCol = methods.extractKeyAndField(columnField);
+  let row: any[];
+  if (keyAndFieldRow.fieldName) {
+    row = gridData.filter(
+      (row) =>
+        row[keyAndFieldRow.key][keyAndFieldRow.fieldName!] === rowValue &&
+        !row.calcRowName
+    );
+  } else {
+    row = gridData.filter(
+      (row) => row[rowField] === rowValue && !row.calcRowName
+    );
+  }
+  if (keyAndFieldCol.fieldName) {
+    return row[0][keyAndFieldCol.key][keyAndFieldCol.fieldName];
+  } else {
+    return row[0][columnField];
+  }
+}
