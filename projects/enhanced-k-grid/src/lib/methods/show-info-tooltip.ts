db9e5cc6c7636957cf17fc1fefc37b06117ab7b0
vargbalazs <balazs.varga.8564@gmail.com>
2024-12-14 11:00:54 +0100

    lib - working on info tooltip 5

---
+++ projects/enhanced-k-grid/src/lib/methods/show-info-tooltip.ts
@@ -0,0 +1,6 @@
+import { InfoTooltip } from '../interfaces/info-tooltip.interface';
+
+// shows the info tooltip
+export function showInfoTooltip(tooltip: InfoTooltip) {
+  console.log(tooltip.name);
+}
