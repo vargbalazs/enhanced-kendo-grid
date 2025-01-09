5189001a18d7d74d16a03f9de70f11ae8cda70c4
vargbalazs <balazs.varga.8564@gmail.com>
2025-01-09 15:50:25 +0100

    working on checkbox column 3

--- src/app/directives/methods/store-grid-body.ts
+++ src/app/directives/methods/store-grid-body.ts
@@ -3,8 +3,10 @@ import { EnhancedGridConfig } from '../classes/enhanced-grid-config.class';
 // store the grid body if we click on a cell (grid body can't be undefined, if we want to copy just one cell)
 export function storeGridBody(
   config: EnhancedGridConfig,
-  e: KeyboardEvent | PointerEvent
+  e: KeyboardEvent | PointerEvent | MouseEvent
 ) {
-  const gridBody = (<HTMLElement>e.target).parentElement?.parentElement;
-  if (gridBody?.hasAttribute('kendogridtablebody')) config.gridBody = gridBody!;
+  const gridBody = (<HTMLElement>config.gridElRef.nativeElement).querySelector(
+    '[kendogridtablebody]'
+  ) as HTMLElement;
+  config.gridBody = gridBody;
 }
