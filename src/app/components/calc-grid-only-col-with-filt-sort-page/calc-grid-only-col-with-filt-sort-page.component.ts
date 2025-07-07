686a020d357584b086c4e2f32dbd7a36824911c9
vargbalazs <balazs.varga.8564@gmail.com>
2025-07-07 15:03:41 +0200

    changing grid config

--- src/app/components/calc-grid-only-col-with-filt-sort-page/calc-grid-only-col-with-filt-sort-page.component.ts
+++ src/app/components/calc-grid-only-col-with-filt-sort-page/calc-grid-only-col-with-filt-sort-page.component.ts
@@ -31,7 +31,7 @@ import { Row } from 'src/app/model/row.model';
   standalone: false,
 })
 export class CalcGridOnlyColWithFiltSortPageComponent implements OnInit {
-  rows: Row[] = inject(DataService).generateData(50);
+  rows: Row[] = inject(DataService).generateData(200);
   rows$!: Observable<Row[]>;
   accountNumbers: AccountNumber[] = accountNumbers;
   projects: Project[] = projects;
